336863bbc7a2d2de0cac9eea8dd02647752f4c79
Iliya Kanchev <iliqtk@gmail.com>
2020-01-17 12:49:39 +0200

    - base structure

---
+++ TheGeneral/TheGeneralCore/OperationSuccessCalculator.cs
@@ -0,0 +1,22 @@
+﻿
+namespace TheGeneralCore
+{
+
+    public class OperationSuccessCalculator : IOperationSuccessCalculator
+    {
+
+        public bool IsAttackSuccesful(int x, int y)
+        {
+            if ((x + y - 5) % 2 == 0) return true;
+
+            return false;
+        }
+
+        public bool IsDefenceSuccesful(int x, int y)
+        {
+            if ((x * y - 5) % 2 == 0) return true;
+
+            return false;
+        }
+    }
+}
---
+++ TheGeneral/TheGeneralTest/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TheGeneralTest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            MainApplication app = new MainApplication();
+            app.Main(ref args);
+        }
+    }
+}
