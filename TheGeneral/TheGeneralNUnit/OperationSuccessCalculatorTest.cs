62a9ebe13f1367ca2dd5263c230004157d306687
Iliya Kanchev <iliqtk@gmail.com>
2020-01-17 13:13:06 +0200

    - added proper unit testing

---
+++ TheGeneral/TheGeneralNUnit/OperationSuccessCalculatorTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using TheGeneralCore;
+
+namespace TheGeneralNUnit
+{
+    [TestFixture()]
+    public class OperationSuccessCalculatorTest
+    {
+        private OperationSuccessCalculator _calc;
+
+        public OperationSuccessCalculatorTest()
+        {
+            _calc = new OperationSuccessCalculator();
+        }
+
+        [Test()]
+        public void TestAttack()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        [Test()]
+        public void TestDefend()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
